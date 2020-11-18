1f451d57896cae1ed89f7f72d4ad1417e770b3b0
Marisa Ramon <mer0522@gmail.com>
2020-11-18 11:24:36 -0600

    enable error messages to show and flash messages upon successful save

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,21 @@ class ArticlesController < ApplicationController
     end
 
     def new
-
+			@article = Article.new
     end
     
-
     def create
-        render plain: params[:article]
-    end
+				# render plain: params[:article]
+				# strong parameters - whitelisting of data that is received through the params hash
+				@article = Article.new(params.require(:article).permit(:title, :description))
+				# render plain: @article.inspect
+				if @article.save
+					flash[:notice] = "Article was created successfully."
+					redirect_to article_path(@article)
+				else 
+					render 'new'
+				end
+		end
+		
 
 end
