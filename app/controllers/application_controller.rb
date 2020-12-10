6f193c6698bbedc898c73fe1c4011424648de010
Marisa Ramon <mer0522@gmail.com>
2020-12-10 14:51:37 -0600

    controller methods as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+     # makes available to views as well as al controllers
+     helper_method :current_user, :logged_in?
+     # to return the details of the user who is logged in
+     def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]       
+    end
+
+     # will simply check if we have a user who is logged in 
+     def logged_in?
+        !!current_user
+    end
+
+
+
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
 				# render plain: params[:article]
 				# strong parameters - whitelisting of data that is received through the params hash
 				@article = Article.new(article_params)
-				@article.user = User.first
+				@article.user = current_user
 				# render plain: @article.inspect
 				if @article.save
 					flash[:notice] = "Article was created successfully."
