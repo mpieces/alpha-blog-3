fec61b87e6ef4ea0f49b8f7bd0bf5ad4f38a1ab3
Marisa Ramon <mer0522@gmail.com>
2020-12-10 14:39:16 -0600

    add authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,15 @@ module ApplicationHelper
         image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
     end
 
+    # to return the details of the user who is logged in
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]       
+    end
+
+    # will simply check if we have a user who is logged in 
+    def logged_in?
+        !!current_user
+    end
+
     
 end
