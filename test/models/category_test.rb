5ca7b6afbcf9f88e1374fa31c36ffef02d673146
Marisa Ramon <mer0522@gmail.com>
2021-06-21 11:55:45 -0500

    use rvm to set ruby version to 3.0, add test file, category model and migration

---
+++ test/models/category_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  
+  test "category should be valid" do 
+    @category  = Category.new(name: "Sports")
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+  end
+
+  test "name should be unique" do 
+  end
+
+  test "name should not be too long" do 
+  end
+
+  test "name should not be too short" do 
+  end
+  
+
+end
+
