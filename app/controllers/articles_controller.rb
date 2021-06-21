b04fd861ad9860bbb015225d20eb2de7657d8ae1
Marisa Ramon <mer0522@gmail.com>
2021-06-21 16:45:42 -0500

    add association from UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,7 @@ class ArticlesController < ApplicationController
 	end
     
     def create
+		byebug
 		# render plain: params[:article]
 		# strong parameters - whitelisting of data that is received through the params hash
 		@article = Article.new(article_params)
@@ -53,7 +54,7 @@ class ArticlesController < ApplicationController
 		end
 
 		def article_params
-			params.require(:article).permit(:title, :description)
+			params.require(:article).permit(:title, :description, category_ids: [])
 		end
 
 		def require_same_user
